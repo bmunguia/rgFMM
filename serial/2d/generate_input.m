33e2f17c2ab8cf8d83d033eb2cec408204ce1d0c
Brian Munguía <bmunguia@stanford.edu>
2016-12-02 11:32:13 -0800

    More updated files

--- serial/2d/generate_input.m
+++ serial/2d/generate_input.m
@@ -1,19 +1,14 @@
-fID = fopen('test1.dat','w');
+fID = fopen('test5.dat','w');
 
-num_part = 4^2
+num_part = 48
 num_dim = num_part^(1/2)
 epsilon = 1e-5
 
 fprintf(fID, 'NUM_PART %i\nMODEL_TYPE GRAVITY\n', num_part);
 fprintf(fID, 'EPSILON %e\n', epsilon);
 
-for i = 0:1:int8(num_dim)-1
-    for j = 0:1:int8(num_dim)-1
-%             x = 1
-%             y = 
-%             z = 
-        fprintf(fID,'PART %f %f %f %f %f\n', 10.0, i, j, 0.0, 0.0);
-    end
+for ii = 1:num_part      
+    fprintf(fID,'PART %f %f %f %f %f\n', randn, rand, rand, 0.0, 0.0);
 end
 
 fclose(fID);
